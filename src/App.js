fea5969859d09976e0595f77d25d9fff80d363f3
Harshithmatta <harshithmatta20@gmail.com>
2024-02-07 14:55:27 +0530

    implemented darkmode/lightmode

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import { useState } from "react";
-import { darkTheme } from './utils/Themes.js'
+import { darkTheme,lightTheme } from './utils/Themes.js'
 import Navbar from "./components/Navbar";
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -26,12 +26,17 @@ const Wrapper = styled.div`
   clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
 `
 function App() {
+  const [darkMode, setDarkMode] = useState(false)
+  const theme = darkMode ? darkTheme : lightTheme;
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
   const [openModal, setOpenModal] = useState({ state: false, project: null });
   console.log(openModal)
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <Router >
-        <Navbar />
+        <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
         <Body>
           <HeroSection />
           <Wrapper>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import {Link as LinkR} from 'react-router-dom';
+import { Link as LinkR } from 'react-router-dom';
 import styled, { useTheme } from "styled-components";
 import { DiCssdeck } from 'react-icons/di';
 import { FaBars } from 'react-icons/fa';
 import { Bio } from '../../data/constants';
+import { lightTheme } from '../../utils/Themes';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import { IconButton, useMediaQuery } from '@mui/material';
 
 
 const Nav = styled.div`
-    background-color: ${({theme}) => theme.card_light};
+    background-color: ${({ theme }) => theme.card_light};
     height: 80px;
     display: flex;
     align-items: center;
@@ -33,6 +37,7 @@ const NavContainer = styled.div`
 `;
 
 const NavLogo = styled(LinkR)`
+    color: ${({ theme }) => theme.text_primary};
     width: 80%;    
     padding: 0 6px;
     display: flex;
@@ -87,7 +92,7 @@ const ButtonContainer = styled.div`
   width: 80%;  
   height: 100%;
   display: flex;
-  justify-content: end;
+  justify-content: space-evenly;
   align-items: center;
   padding: 0 6px;
   @media screen and (max-width: 768px) {
@@ -118,11 +123,14 @@ export const GitHubButton = styled.a`
     }
 `;
 
+
 const Span = styled.div`
     padding: 0 4px;
     font-weight: bold;
     font-size: 18px;
+    color: ${({ theme }) => theme.text_primary}
 `;
+
 
 
 const MobileMenu = styled.div`
@@ -135,7 +143,7 @@ const MobileMenu = styled.div`
     right: 0;
     width: 100%;
     padding: 12px 40px 24px 40px;
-    background: ${({ theme }) => theme.card_light+99};
+    background: ${({ theme }) => theme.card_light + 99};
     transition: all 0.6s ease-in-out;
     transform: ${({ open }) => (open ? 'translateX(0)' : 'translateX(100%)')};
     border-radius: 0 0 20px 20px;
@@ -173,61 +181,68 @@ export const MobileMenuLinks = styled(LinkR)`
 
 
 const Navbar
- = () => {
-  const [open, setopen] = React.useState(false);
-  const theme = useTheme();
-  return (<Nav>
-    <NavContainer>
-    <NavLogo to='/'>
-          <a href="/" style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer', textDecoration: 'None'}}>
-            <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+  = ({ darkMode, toggleDarkMode }) => {
+    const isSmallScreen = useMediaQuery('(max-width:768px)')
+    const [open, setopen] = React.useState(false);
+    const theme = useTheme();
+    return (<Nav>
+      <NavContainer>
+        <NavLogo to='/' theme={lightTheme} >
+          <a href="/" style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer', textDecoration: 'None' }}>
+            <DiCssdeck style={{ color: theme.primary }} size="3rem" /> <Span>Portfolio</Span>
           </a>
         </NavLogo>
+        {isSmallScreen && (<IconButton color="inherit" onClick={toggleDarkMode} style={{ marginTop: "-6px", right: "40px" }}>
+          {darkMode ? <Brightness7Icon style={{ fontSize: 30 , color: 'white' }} /> : <Brightness4Icon style={{ fontSize: 30 }} />}
+        </IconButton>)}
         <MobileIcon>
           <FaBars onClick={() => {
             setopen(!open)
           }} />
         </MobileIcon>
         <NavItems>
-        <NavLink href="#about">About</NavLink>
-        <NavLink href='#skills'>Skills</NavLink>
-        <NavLink href='#experience'>Experience</NavLink>
-        <NavLink href='#projects'>Projects</NavLink>
-        <NavLink href='#education'>Education</NavLink>
+          <NavLink href="#about">About</NavLink>
+          <NavLink href='#skills'>Skills</NavLink>
+          <NavLink href='#experience'>Experience</NavLink>
+          <NavLink href='#projects'>Projects</NavLink>
+          <NavLink href='#education'>Education</NavLink>
         </NavItems>
         <ButtonContainer>
           <GitHubButton href={Bio.github} target="_blank" >
             GitHub Profile
           </GitHubButton>
+          {!isSmallScreen && (<IconButton color="inherit" onClick={toggleDarkMode}>
+            {darkMode ? <Brightness7Icon style={{ fontSize: 32, color: 'white' }} /> : <Brightness4Icon style={{ fontSize: 32 }} />}
+          </IconButton>)}
         </ButtonContainer>
 
         {
-      open && (
-        <MobileMenu open={open}>
-            <MobileMenuLinks href="#about" onClick={() => {
-              setopen(!open)
-            }}>About</MobileMenuLinks>
-            <MobileMenuLinks href='#skills' onClick={() => {
-              setopen(!open)
-            }}>Skills</MobileMenuLinks>
-            <MobileMenuLinks href='#experience' onClick={() => {
-              setopen(!open)
-            }}>Experience</MobileMenuLinks>
-            <MobileMenuLinks href='#projects' onClick={() => {
-              setopen(!open)
-            }}>Projects</MobileMenuLinks>
-            <MobileMenuLinks href='#education' onClick={() => {
-              setopen(!open)
-            }}>Education</MobileMenuLinks>
-            <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.github} target="_blank">Github Profile</GitHubButton>
-        </MobileMenu>
-      )
-    }
-    </NavContainer>
-    
-  </Nav>
-  )
-}
+          open && (
+            <MobileMenu open={open}>
+              <MobileMenuLinks href="#about" onClick={() => {
+                setopen(!open)
+              }}>About</MobileMenuLinks>
+              <MobileMenuLinks href='#skills' onClick={() => {
+                setopen(!open)
+              }}>Skills</MobileMenuLinks>
+              <MobileMenuLinks href='#experience' onClick={() => {
+                setopen(!open)
+              }}>Experience</MobileMenuLinks>
+              <MobileMenuLinks href='#projects' onClick={() => {
+                setopen(!open)
+              }}>Projects</MobileMenuLinks>
+              <MobileMenuLinks href='#education' onClick={() => {
+                setopen(!open)
+              }}>Education</MobileMenuLinks>
+              <GitHubButton style={{ padding: '10px 16px', background: `${theme.primary}`, color: 'white', width: 'max-content' }} href={Bio.github} target="_blank">Github Profile</GitHubButton>
+            </MobileMenu>
+          )
+        }
+      </NavContainer>
+
+    </Nav>
+    )
+  }
 
 export default Navbar;
 
